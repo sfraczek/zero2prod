9ecc2a3a9a9f09654c7a0e688154836cd73c9a7d
Sylwester Fraczek <s.fraczek@gmail.com>
2024-10-05 21:46:22 +0200

    fixes and splitting project to files

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -7,7 +7,7 @@ if ! [ -x "$(command -v psql)" ]; then
 	exit 1
 fi
 
-if ! [ -x "$(command -v psql)" ]; then
+if ! [ -x "$(command -v sqlx)" ]; then
 	echo >&2 "Error: sqlx is not installed."
 	echo >&2 "Use:"
 	echo >&2 "    cargo install --version='~0.7' sqlx-cli \
